bccca5f08f49abf6da8e116850c677dfbae64450
VitorRecalcatti <133670390+VitorRecalcatti@users.noreply.github.com>
2023-05-15 11:00:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function calendar() {
+    var nome = document.getElementById(nome);
+    var matricula = document.getElementById(siape);
+    var contato = document.getElementById(email);
+    var area_formada = document.getElementById(area);
+    var carga_horaria = document.getElementById(carga_horaria)
+}
